eb514a290049f442094e1f87325851f1dd34e7f3
Your Name <hayjclove@gmail.com>
2021-07-22 01:07:05 -0400

    GG

---
+++ src/Boro.java
@@ -0,0 +1,11 @@
+public class Boro {
+
+    public static void main(String[]args) {
+
+int y=9;
+for(int g = 2; g <= 5; g++){
+    System.out.println(g);
+    
+}
+    }
+}
---
+++ src/Gosh.java
@@ -0,0 +1,65 @@
+//import java.sql.Array;
+//import java.util.ArrayList;
+//
+//public class Gosh{
+//    private static final String james = "";
+//   // only gosh class run it not the other one wor wrapper class
+//    private Array [] wor;
+//    public ArrayList<Integer> cocaCola;      // is accessible for the Gosh class and the sub class
+//    public static void run(){
+//     //int x = null; // primitive
+//   String xx = null;
+//   ArrayList <Integer> cocaCola = null;
+//   Array[] wor = null;
+//   }
+//
+//    public static void main(String[]args){
+//        run();
+//       String comfortable = "degezaw guy is playing with his phone";
+//       String f = comfortable.toUpperCase();
+//       System.out.println(f);
+//       // String buffer
+//       StringBuffer buffer = new StringBuffer("yEHelinaEsregna");  //muttable we can upgrade it // changeit
+//       // StringBuffer
+//       StringBuffer x = buffer.reverse();
+//       int y = buffer.length(); // length => counted with numbers each character
+//       System.out.println(x + ", and the length: " + y);
+//
+//    }
+//    // static vs non static method
+//    // static is a key word; it calling is for the class; you can`t instantiate;
+//    // non static you can instantiate it and add it to the wrapper class and oop class;
+//    //
+//
+//    String cake = "She is out of my league";
+////         switch(cake){
+////
+////          case 1: dffdff {
+////              System.out.println("Hello baby");
+////              break;
+////          }
+////
+////          case 2: dsdfdf {
+////              System.out.println("Hello yes baby2");
+////              break;
+////
+////          }
+////
+////          default:
+////              System.out.println("Hello bad boys: ");
+////              break;
+////
+////         }
+//
+//
+//
+//      }
+//
+//
+//
+//
+//
+//
+//}
+
+
